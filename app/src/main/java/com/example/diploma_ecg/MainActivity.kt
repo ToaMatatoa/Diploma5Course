09d7eb9b2656fdd23f8b9ff1af1792c13e993648
Matatoa <rikonikotako@gmail.com>
2020-10-02 17:56:09 +0300

    changed MainActivity.kt, bottom_nav_menu.xml, strings.xml, AndroidManifest.xml, build.gradle

--- app/src/main/java/com/example/diploma_ecg/MainActivity.kt
+++ app/src/main/java/com/example/diploma_ecg/MainActivity.kt
@@ -1,14 +1,15 @@
-package com.example.coronanews
+package com.example.diploma_ecg
 
 import android.os.Bundle
+import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isGone
 import androidx.fragment.app.Fragment
-import com.example.coronanews.bookmark.ui.FavouriteFragment
-import com.example.coronanews.graph.ui.GraphFragment
-import com.example.coronanews.news.ui.NewsFragment
+import com.example.diploma_ecg.bookmark.ui.FavouriteFragment
+import com.example.diploma_ecg.graph.ui.GraphFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity() {
+open class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -16,15 +17,26 @@ class MainActivity : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.corona_news)
-        val bottomNav: BottomNavigationView = findViewById(R.id.bottom_navigation)
-        bottomNav.itemIconTintList = null
-        bottomNav.setOnNavigationItemSelectedListener(navListener)
+
+        val bottomNavigation: BottomNavigationView = findViewById(R.id.bottom_navigation)
+        bottomNavigation.itemIconTintList = null
+        bottomNavigation.setOnNavigationItemSelectedListener(navListener)
 
         if (savedInstanceState == null) {
+            bottomNavigation.isGone = true
             supportFragmentManager.beginTransaction().replace(
                 R.id.fragment_container,
-                NewsFragment()
+                SplashFragment()
             ).commit()
+
+            val h = Handler()
+            h.postDelayed(Runnable {
+                bottomNavigation.isGone = false
+                supportFragmentManager.beginTransaction().replace(
+                    R.id.fragment_container,
+                    GraphFragment()
+                ).commit()
+            }, 2500)
         }
     }
 
@@ -34,8 +46,6 @@ class MainActivity : AppCompatActivity() {
             when (item.itemId) {
                 R.id.menu_graph -> selectedFragment =
                     GraphFragment()
-                R.id.menu_news -> selectedFragment =
-                    NewsFragment()
                 R.id.menu_favourite -> selectedFragment =
                     FavouriteFragment()
             }
